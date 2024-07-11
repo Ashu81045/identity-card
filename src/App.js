1c0e6135fd0c1504da3e8ac13e49aae90a93372d
Bakhtiyar <asim.bakhtiyar@accenture.com>
2024-07-12 02:04:46 +0530

    Adding no service page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
-import HomeLayout from './Layout/HomeLayout';
-import TrackPage from './pages/TrackPage';
-import AdminPage from './pages/AdminPage';
-import ContactPage from './pages/ContactPage';
+// import HomeLayout from './Layout/HomeLayout';
+// import TrackPage from './pages/TrackPage';
+// import AdminPage from './pages/AdminPage';
+// import ContactPage from './pages/ContactPage';
+import NoService from './pages/NoService';
 
 
 function App() {
@@ -12,11 +13,13 @@ function App() {
       <Router>
         {/* Use Routes component to define routes */}
         <Routes>
-          {/* Use Route component to define individual routes */}
-          <Route path="/" element={<HomeLayout />} /> {/* Use element prop instead of component */}
+        <Route path="/" element={<NoService />} />
+          {/* Use Route component to define individual routes 
+          <Route path="/" element={<HomeLayout />} /> 
           <Route path="/track" element={<TrackPage />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          */}
         </Routes>
       </Router>
     </div>
---
+++ src/pages/NoService.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "../Layout/Header";
+import "./TrackingPage.css";
+
+
+const NoService = () => {
+ 
+
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div style={{ margin: "1rem" }}>
+        <div className="tracking-container">
+          <h2 className="tracking-heading">Free servicesd have been terminated, please contact admin for help.</h2>
+          <p>Thank you, have a good day!!</p>
+          </div>
+          </div>
+    </React.Fragment>
+  );
+};
+
+export default NoService;
